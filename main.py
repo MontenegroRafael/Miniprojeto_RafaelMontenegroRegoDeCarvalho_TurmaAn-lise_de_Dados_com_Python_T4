29fa58a96ad5f94f6a72c7a93770967d52d12b51
Rafael <rafaelmrc3@gmail.com>
2026-08-16 18:29:36 -0300

    Limpeza de dados

--- main.py
+++ main.py
@@ -1,18 +1,69 @@
+# importa a biblioteca pandas.
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
-# Carrega o arquivo csv / função que lê um arquivo de texto separado por vírgulas e o transforma em DataFrame.
-df = pd.read_csv('BaseVarejo.csv')  #>Lê todo o arquivo
-# df = pd.read_csv('BaseVarejo.csv', usecols=['valor', 'data_transacao']) #>Lê apenas as colunas selecionadas
-# Mostra só as primeiras n linhas, útil para espiar a tabela sem imprimir tudo. head(n)
 
-#df = pd.read_csv('BaseVarejo.csv', nrows=0)
+# Carrega o arquivo csv / transforma arquivo em um DataFrame. 
+  # sep=';': Define o ponto e vírgula (;)
+  # encoding='utf-8-sig': Lê o arquivo com acentos corretos da língua portuguesa e remove marcas invisíveis no início do arquivo.
+  # parse_dates=['DATA']: Converte a coluna 'DATA' para o tipo datetime.
+  # dayfirst=True: Define que o formato da data é dia/mês/ano.
+df = pd.read_csv('BaseVarejo.csv', sep=';', encoding='utf-8-sig', parse_dates=['DATA'], dayfirst=True,) 
 
-# Mostra todas as colunas
+print(df.head(10)) # Mostrar as primeiras 10 linhas
+print(df.info()) # tipos e nulos por coluna
+
+
+#criação de um dicionário para renomear as colunas do DataFrame
+converção_colunas = {
+    'DATA': 'dt_Compra',
+    'CO_ID': 'id_Compra',
+    'CL_ID': 'id_Cliente',
+    'CL_GENERO': 'genero_Cliente',
+    'CL_EC': 'estadoCivil_Cliente',
+    'CL_FHL': 'filhos_Cliente',
+    'CL_SEG': 'segmento_Cliente',
+    'PR_ID': 'id_Produto',
+    'PR_CAT': 'categoria_Produto',
+    'PR_NOME': 'nome_Produto',
+}
+df=df.rename(columns=converção_colunas) #função renomear colunas com base no dicionário criado acima.
+df.columns
+
+print("Valores nulos por coluna:")
+print(df.isnull().sum()) # Mostrar a quantidade de valores nulos por coluna
+
+
+# Remove colunas que estão completamente em branco
+  # usa how='all' para apagar a coluna que estiver totalmnete vazia
+  # axis=1 para apagar a coluna, caso fosse axis=0 apagaria a linha
+df_limpo = df.dropna(how='all', axis=1)
+
+# Salvar em um novo arquivo CSV (sem a coluna de índice)
+df_limpo.to_csv('BaseVarejoLimpo.csv', index=False)
+print(df_limpo.head(10)) # Mostrar as primeiras 10 linhas do novo arquivo limpo
+
+'''
+
+#, usecols=['DATA', 'CO_ID', 'CL_ID', 'CL_GENERO', 'CL_EC', 'CL_FHL', 'CL_SEG', 'PR_ID', 'PR_CAT', 'PR_NOME']
+#, index_col='DATA'
+df.describe() # Mostra estatísticas descritivas do DataFrame
+print(df.describe()) # Mostra estatísticas descritivas do DataFrame
+print("\nInformações das colunas:")
+print(df.info())
+print("\nValores nulos por coluna:")
+print(df.isnull().sum())
+
+df = pd.read_csv('BaseVarejo.csv', nrows=0)
+
+
 pd.set_option("display.max_columns", None)
 print(df.iloc[:, -3:].head())
 
-print(df.head(10))
-#print(df.info()) # tipos e nulos por coluna
+
+print(df.info()) # tipos e nulos por coluna
 
 
 print(df.columns)
+'''
